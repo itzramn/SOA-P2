ea6258eabfa31a8f36be3962059fb60e9a077f57
Itzel Ramon <itzelramonf@gmail.com>
2023-06-14 12:53:25 -0500

    Se han añadido las rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./components/Auth";
 import Employees from "./pages/Employees";
 import Assets from "./pages/Assets";
 
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="empleados" element={<Employees />} />'
+        <Route path="activos" element={<Assets />} />
+        <Route path="*" element={<div>404 not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
