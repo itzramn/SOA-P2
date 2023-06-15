8caba46021416642c0151359a2c2ca58ebb9f286
Diego <diegogutcat28@gmail.com>
2023-06-15 11:52:17 -0500

    Login añadido

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -1,6 +1,8 @@
-import { soapResponse } from '../utils';
+import axios from 'axios';
+import { wcfUrl } from '../apiUrl';
 
 export const ValidateEmployee = async user => {
+   console.log(user);
    const wcfMethod = 'ValidateEmployeeLogin';
 
    const xmlBodyStr = `<?xml version="1.0" encoding="utf-8"?>
@@ -13,6 +15,29 @@ export const ValidateEmployee = async user => {
   </soap:Body>
 </soap:Envelope>`;
 
-   const response = await soapResponse(wcfMethod, xmlBodyStr, null);
-   return response;
+   const config = {
+      headers: {
+         'Content-Type': 'text/xml; charset=utf-8',
+         SOAPAction: `http://tempuri.org/IService/${wcfMethod}`,
+      },
+   };
+
+   try {
+      const response = await axios.post(wcfUrl, xmlBodyStr, config);
+
+      const parser = new DOMParser();
+      const xmlDOM = parser.parseFromString(response.data, 'application/xml');
+
+      const status = xmlDOM.getElementsByTagName(`${wcfMethod}Result`)[0]
+         .childNodes[0].nodeValue;
+
+      console.log(status);
+
+      if (status !== 'OK') return false;
+
+      return true;
+   } catch (error) {
+      console.log(error);
+      return true;
+   }
 };
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,64 +1,71 @@
-import React, {useState} from "react";
-import "../styles/modal.css";
-import {ValidateEmployee} from "../api/auth.api";
+import React, { useState } from 'react';
+import '../styles/modal.css';
+import { ValidateEmployee } from '../api/auth.api';
+import { useNavigate } from 'react-router-dom';
 
 const Auth = () => {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
+   const [user, setUser] = useState({
+      email: '',
+      password: '',
+   });
 
-  const handleKeys = (field, value) => {
-    setUser((prevUser) => ({
-      ...prevUser,
-      [field]: value,
-    }));
-  };
+   const navigate = useNavigate();
 
-  const handleAuth = async (user) => {
-    await ValidateEmployee(user);
-    //falta el redireccionameinto
-  };
+   const handleKeys = (field, value) => {
+      setUser(prevUser => ({
+         ...prevUser,
+         [field]: value,
+      }));
+   };
 
-  return (
-    <div className="auth-container">
-      <div className="container" style={{width: "30rem"}}>
-        <h3>Inicia sesión</h3>
-        <div className="input-form-content row full">
-          <div className="column full">
-            <label className="form-label">Correo</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="name@example.com"
-              value={user.email}
-              onChange={(e) => handleKeys("email", e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="input-form-content row full">
-          <div className="column full">
-            <label className="form-label">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Escribe tu contraseña"
-              aria-labelledby="passwordHelpBlock"
-              value={user.password}
-              onChange={(e) => handleKeys("password", e.target.value)}
-            />
-          </div>
-        </div>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={() => handleAuth(user)}
-        >
-          Ingresar
-        </button>
+   const handleAuth = async user => {
+      const isValidUser = await ValidateEmployee(user);
+      if (!isValidUser) {
+         return alert('Credenciales incorrectas');
+      }
+      navigate('empleados');
+      //falta el redireccionameinto
+   };
+
+   return (
+      <div className="auth-container">
+         <div className="container" style={{ width: '30rem' }}>
+            <h3>Inicia sesión</h3>
+            <div className="input-form-content row full">
+               <div className="column full">
+                  <label className="form-label">Correo</label>
+                  <input
+                     type="email"
+                     className="form-control"
+                     placeholder="name@example.com"
+                     value={user.email}
+                     onChange={e => handleKeys('email', e.target.value)}
+                  />
+               </div>
+            </div>
+            <div className="input-form-content row full">
+               <div className="column full">
+                  <label className="form-label">Password</label>
+                  <input
+                     type="password"
+                     className="form-control"
+                     placeholder="Escribe tu contraseña"
+                     aria-labelledby="passwordHelpBlock"
+                     value={user.password}
+                     onChange={e => handleKeys('password', e.target.value)}
+                  />
+               </div>
+            </div>
+            <button
+               className="btn btn-primary"
+               type="button"
+               onClick={() => handleAuth(user)}
+            >
+               Ingresar
+            </button>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Auth;
